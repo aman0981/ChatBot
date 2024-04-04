f3a5eb4c4dc9217169f26b69a5f34e8b5fae01e2
Aman Nikumb <99965562+aman0981@users.noreply.github.com>
2024-04-05 01:56:54 +0530

    third commit

--- app/src/main/java/com/example/chatbot/API_KEY.kt
+++ app/src/main/java/com/example/chatbot/API_KEY.kt
@@ -1,6 +1,12 @@
 package com.example.chatbot
 
+import com.example.chatbot.API_KEY.MY_API_KEY
+import com.google.ai.client.generativeai.Chat
+import com.google.ai.client.generativeai.GenerativeModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 object API_KEY {
 
-    const val MY_API_KEY = "Your_Api_Key"
+    const val MY_API_KEY = "Your Gemini API"
 }
